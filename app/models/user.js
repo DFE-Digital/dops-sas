d96927f87a4ff18eb6d31f4972941eb920a53ac3
Andy Jones <andy.jones@digital.education.gov.uk>
2024-03-17 15:30:51 +0000

    prod encryption

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
- // ssl: { rejectUnauthorized: false }
+ ssl: { rejectUnauthorized: false }
 });
 
 /**
